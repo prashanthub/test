9541e905ff4cfb044ac29a0e746fd5cbe3bc781d
naobi <naobi71@gmail.com>
2020-02-02 16:49:13 +0530

    chages

--- index.php
+++ index.php
@@ -1,2 +1,79 @@
 <?php
-phpinfo();
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+
+
+/****************  List Buckets  *************/
+$s3Client = new S3Client([
+    'profile' => 'default',
+    'region' => 'us-west-2',
+    'version' => '2006-03-01'
+]);
+
+//Listing all S3 Bucket
+$buckets = $s3Client->listBuckets();
+foreach ($buckets['Buckets'] as $bucket) {
+    echo $bucket['Name'] . "\n";
+}
+ 
+die; 
+/**************** Create a Bucket ***********/
+
+$BUCKET_NAME = '<BUCKET-NAME>';
+
+//Create a S3Client
+$s3Client = new S3Client([
+    'profile' => 'default',
+    'region' => 'us-west-2',
+    'version' => '2006-03-01'
+]);
+
+//Creating S3 Bucket
+try {
+    $result = $s3Client->createBucket([
+        'Bucket' => $BUCKET_NAME,
+    ]);
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
+
+/*******************Put an Object in a Bucket******************/
+
+$USAGE = "\n" .
+    "To run this example, supply the name of an S3 bucket and a file to\n" .
+    "upload to it.\n" .
+    "\n" .
+    "Ex: php PutObject.php <bucketname> <filename>\n";
+
+if (count($argv) <= 2) {
+    echo $USAGE;
+    exit();
+}
+
+$bucket = $argv[1];
+$file_Path = $argv[2];
+$key = basename($argv[2]);
+
+try {
+    //Create a S3Client
+    $s3Client = new S3Client([
+        'profile' => 'default',
+        'region' => 'us-west-2',
+        'version' => '2006-03-01'
+    ]);
+    $result = $s3Client->putObject([
+        'Bucket' => $bucket,
+        'Key' => $key,
+        'SourceFile' => $file_Path,
+    ]);
+} catch (S3Exception $e) {
+    echo $e->getMessage() . "\n";
+}
+ 
+ 
+
